d80711fd1984b50726aab69831f5ac373b87dfc5
Teo971 <frateo971@hotmail.com>
2024-11-08 17:37:48 +0100

    Affichage des données d'entrainements

--- Test GAN.py
+++ Test GAN.py
@@ -77,11 +77,12 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
 # Decide which device we want to run on
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
-# Plot some training images
-real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
-plt.axis("off")
-plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-plt.show()
-
+if __name__ == "__main__":
+    # Your existing code goes here
+    # For example:
+    real_batch = next(iter(dataloader))
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0)))
+    plt.show()
