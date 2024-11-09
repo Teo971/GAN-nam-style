d7beb3ebcc9527761bc52a3ec02fff09601fd7d4
unicorn <118615352+ZixinP@users.noreply.github.com>
2024-11-09 14:52:08 +0100

    Add files via upload

---
+++ modèle.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, noise_dim=100, out_channels=3):
+        super(Generator, self).__init__()
+        
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(noise_dim, 512, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(64, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3):
+        super(Discriminator, self).__init__()
+        
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()  
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+noise_dim = 100      # bruit
+out_channels = 3     # RGB
+
+generator = Generator(noise_dim=noise_dim, out_channels=out_channels)
+discriminator = Discriminator(in_channels=out_channels)
+
+# test
+noise = torch.randn((1, noise_dim, 1, 1))   # bruit aleatoire
+fake_image = generator(noise)               
+real_or_fake = discriminator(fake_image)    
+
+print("taille d'image generee：", fake_image.shape)       # torch.Size([1, 3, 64, 64])
+print("taille de output de discriminateur：", real_or_fake.shape)    # torch.Size([1, 1, 1, 1])
