5fc47ba85b9787a5f60208a4b62f177ac64e1515
Teo971 <frateo971@hotmail.com>
2024-11-23 16:40:29 +0100

    tentative avec accuracy

--- Test GAN.py
+++ Test GAN.py
@@ -30,12 +30,13 @@ torch.use_deterministic_algorithms(True) # Needed for reproducible results
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Root directory for dataset
-dataroot = Path("C:/Users/frate/OneDrive/Bureau/Insa/TC/4TC/TIP/DataSet/Abjad.v1i.yolov7pytorch/train/Test images")
+dataroot_1 = Path("c:/Users/frate/OneDrive/Bureau/Insa/TC/4TC/TIP/DataSet/equine emotion detection.v1i.multiclass/Test_folder")
+dataroot_2 = Path("c:/Users/frate/OneDrive/Bureau/Insa/TC/4TC/TIP/DataSet/equine emotion detection.v1i.multiclass/Train_folder")
 # Number of workers for dataloader
 workers = 2
 
 # Batch size during training
-batch_size = 128
+batch_size = 16
 
 # Spatial size of training images. All images will be resized to this size using a transformer.
 image_size = 64
@@ -53,7 +54,7 @@ ngf = 64
 ndf = 64
 
 # Number of training epochs
-num_epochs = 100
+num_epochs = 10
 
 # Learning rate for optimizers
 lr = 0.0002
@@ -65,7 +66,15 @@ beta1 = 0.5
 ngpu = 1
 # We can use an image folder dataset the way we have it setup.
 # Create the dataset
-dataset = dset.ImageFolder(root=dataroot,
+test_dataset = dset.ImageFolder(root=dataroot_1,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+train_dataset = dset.ImageFolder(root=dataroot_2,
                            transform=transforms.Compose([
                                transforms.Resize(image_size),
                                transforms.CenterCrop(image_size),
@@ -73,8 +82,12 @@ dataset = dset.ImageFolder(root=dataroot,
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
 # Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
                                          shuffle=True, num_workers=workers)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
 
 # Decide which device we want to run on
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
@@ -88,13 +101,14 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
+    
 """real_batch = next(iter(dataloader))
-    plt.figure(figsize=(8,8))
-    plt.axis("off")
-    plt.title("Training Images")
-    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0)))
-    plt.show()"""
-
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0)))
+plt.show()
+"""
 if __name__ == "__main__":
     # Your existing code goes here
     # For example:
@@ -135,8 +149,8 @@ if __name__ == "__main__":
     fixed_noise = torch.randn(64, nz, 1, 1, device=device)
 
     # Establish convention for real and fake labels during training
-    real_label = 1.
-    fake_label = 0.
+    real_label = 0.9
+    fake_label = 0
 
     # Setup Adam optimizers for both G and D
     optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
@@ -155,7 +169,7 @@ if __name__ == "__main__":
     # For each epoch
     for epoch in range(num_epochs):
         # For each batch in the dataloader
-        for i, data in enumerate(dataloader, 0):
+        for i, data in enumerate(train_loader, 0):
 
             ############################
             # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
@@ -210,7 +224,7 @@ if __name__ == "__main__":
             # Output training stats
             if i % 50 == 0:
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                    % (epoch, num_epochs, i, len(dataloader),
+                    % (epoch, num_epochs, i, len(train_loader),
                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
             # Save Losses for plotting later
@@ -218,12 +232,53 @@ if __name__ == "__main__":
             D_losses.append(errD.item())
 
             # Check how the generator is doing by saving G's output on fixed_noise
-            if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+            if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(train_loader)-1)):
                 with torch.no_grad():
                     fake = netG(fixed_noise).detach().cpu()
                 img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
             iters += 1
+    torch.save(netG.state_dict(), "generator1.pth")
+    torch.save(netD.state_dict(), "discriminator1.pth")
+
+
+    """torch.save(netG, "generator_full.pth")
+    torch.save(netD, "discriminator_full.pth")"""
+    
+    def calculate_accuracy(model, dataloader, device):
+        """
+        Calculate accuracy of a model on a given dataloader.
+        
+        Args:
+            model (torch.nn.Module): The trained discriminator model.
+            dataloader (torch.utils.data.DataLoader): DataLoader for the dataset (e.g., test_loader).
+            device (torch.device): The device on which the model and data reside.
+        
+        Returns:
+            float: Accuracy of the model as a percentage.
+        """
+        model.eval()  # Set the model to evaluation mode
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for data in dataloader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+
+                # Forward pass: get predictions
+                outputs = model(images).view(-1)
+
+                # Threshold predictions (e.g., 0.5 for binary classification)
+                predicted = (outputs >= 0.5).float()  # Predicted as real (1) if >= 0.5, else fake (0)
+
+                # Binary labels: 1 for real, 0 for fake
+                correct += (predicted == labels.float()).sum().item()
+                total += labels.size(0)
+
+        accuracy = 100 * correct / total
+        return accuracy
+    accuracy = calculate_accuracy(netD, test_loader, device)
     plt.subplot(1,2,2)
     plt.axis("off")
     plt.title("Fake Images")
@@ -239,3 +294,4 @@ if __name__ == "__main__":
     plt.ylabel("Loss")
     plt.legend()
     plt.show()
+
